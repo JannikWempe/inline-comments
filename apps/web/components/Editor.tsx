3d1aaf4dc3c4fc60c13fdd9c0be94f901a53f79d
Jannik Wempe <jannik.wempe@iteratec.com>
2022-02-10 06:29:00 +0100

    fix(web): not updating comments if a marker was removed

--- apps/web/components/Editor.tsx
+++ apps/web/components/Editor.tsx
@@ -1,7 +1,8 @@
 import type { ReactElement } from "react";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useDebounce } from "react-use";
-import { PostFragment, useUpdatePostMutation } from "../lib/api/api.generated";
+import { useQueryClient } from "react-query";
+import { PostFragment, usePostQuery, useUpdatePostMutation } from "../lib/api/api.generated";
 import { useApplyCommentMarkerConnection } from "../hooks/use-apply-comment-marker-connection";
 
 type Props = {
@@ -9,37 +10,49 @@ type Props = {
   className?: string;
 };
 
-export const Editor = ({ post, className }: Props): ReactElement => {
-  const [title, setTitle] = useState(() => post.title);
-  const [content, setContent] = useState(() => post.content);
-  useApplyCommentMarkerConnection();
-  const updatePostMutation = useUpdatePostMutation();
+export const Editor = React.memo(
+  ({ post, className }: Props): ReactElement => {
+    const [title, setTitle] = useState(() => post.title);
+    const [content, setContent] = useState(() => post.content);
+    useApplyCommentMarkerConnection();
+    const queryClient = useQueryClient();
+    const updatePostMutation = useUpdatePostMutation({
+      onSuccess: () => {
+        queryClient.invalidateQueries(usePostQuery.getKey({ id: post.id }));
+      },
+    });
 
-  useDebounce(
-    () => {
-      updatePostMutation.mutate({ id: post.id, title, content });
-    },
-    1000,
-    [title, content]
-  );
+    const contentRef = useRef<HTMLPreElement>(null);
 
-  return (
-    <div className={`flex flex-col space-y-3 items-center ${className}`}>
-      <input
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.currentTarget.value)}
-        className="w-full p-2 font-semibold rounded-lg border-solid border-2 border-gray-100 focus:outline-2"
-      />
-      {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
-      <pre
-        className="highlight-comments w-full p-2 whitespace-pre-wrap rounded-lg border-solid border-2 border-gray-100 selection:bg-blue-500/20"
-        contentEditable
-        onInput={(e) => {
-          setContent(e.currentTarget.innerHTML);
-        }}
-        dangerouslySetInnerHTML={{ __html: post.content }}
-      />
-    </div>
-  );
-};
+    useDebounce(
+      () => {
+        updatePostMutation.mutate({ id: post.id, title, content });
+      },
+      1000,
+      [title, content]
+    );
+
+    return (
+      <div className={`flex flex-col space-y-3 items-center ${className}`}>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.currentTarget.value)}
+          className="w-full p-2 font-semibold rounded-lg border-solid border-2 border-gray-100 focus:outline-2"
+        />
+        {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions  */}
+        <pre
+          ref={contentRef}
+          className="highlight-comments w-full p-2 whitespace-pre-wrap rounded-lg border-solid border-2 border-gray-100 selection:bg-blue-500/20"
+          contentEditable
+          onInput={(e) => {
+            setContent(e.currentTarget.innerHTML);
+          }}
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
+      </div>
+    );
+  },
+  // excluding content from rerendering this component; a rerender leads to cursor jumping
+  (prev, next) => prev.post.id === next.post.id && prev.post.comments.length === next.post.comments.length
+);
