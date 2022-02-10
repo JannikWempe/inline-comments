84ca8892e758f246c072425845efa61c3d3af162
Jannik Wempe <jannik.wempe@iteratec.com>
2022-02-10 18:23:24 +0100

    fix(web): purging hover marker tailwind class

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -2,8 +2,10 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./hooks/**/*.{js,ts,jsx,tsx}",
     '../../packages/ui/**/*.{js,ts,jsx,tsx}',
   ],
+  // safelist: ["!bg-red-500/50", "bg-red-500/50"],
   theme: {
     extend: {},
   },
